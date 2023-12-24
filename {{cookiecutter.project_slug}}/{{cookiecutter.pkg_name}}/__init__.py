12decdb3c2e4d94f024c92b46fb76468cf3d4a99
Bram Enning <bramenning@gmail.com>
2023-12-24 13:36:07 +0100

    created new variable for config-dir name

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
@@ -11,7 +11,8 @@ from pathlib import Path
 logger = logging.getLogger(__name__)
 
 APP_NAME = '{{ cookiecutter.pkg_name }}'
-APP_DIR = Path.home() / "." + APP_NAME
+DOT_APP_NAME = '.' + APP_NAME
+APP_DIR = Path.home() / DOT_APP_NAME
 # create config directory if it does not exist.
 
 Path(APP_DIR).mkdir(parents=True, exist_ok=True)
