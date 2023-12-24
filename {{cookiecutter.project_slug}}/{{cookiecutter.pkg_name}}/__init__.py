8506711f60fd96490aab6c940d01238aa7ff4d31
Bram Enning <bramenning@gmail.com>
2023-12-24 13:05:02 +0100

    added basic config file and typer

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
@@ -3,3 +3,21 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
+
+
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+APP_NAME = '{{ cookiecutter.pkg_name }}'
+APP_DIR = Path.home() / "." + APP_NAME
+# create config directory if it does not exist.
+
+Path(APP_DIR).mkdir(parents=True, exist_ok=True)
+
+CONFIG_FILE = APP_DIR / "config.INI"
+
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+
+logging.basicConfig(format=FORMAT, level=logging.INFO)
--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/cli.py
@@ -14,4 +14,28 @@ def main():
 
 if __name__ == "__main__":
     main()  # pragma: no cover
+
+{% elif cookiecutter.command_line_interface|lower == 'typer' -%}
+import typer
+from {{ cookiecutter.pkg_name }} import CONFIG_FILE
+
+app = typer.Typer(
+    name="{{ cookiecutter.pkg_name }}",
+    help="{{ cookiecutter.project_short_description }}",
+)
+
+@app.callback()
+def callback():
+    """
+    {{ cookiecutter.project_short_description }}
+    """
+
+# create commandline tool with code like below:
+@app.command()
+def example_command():
+    typer.echo(
+        f"This will print the config file path: {CONFIG_FILE} "
+    )
+
+
 {%- endif %}
