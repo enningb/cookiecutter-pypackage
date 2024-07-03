9df693d4968353de5d0795e62c6969f0a072e4c0
Bram Enning <bramenning@gmail.com>
2024-07-03 10:43:54 +0200

    updated from eencijfer-py

--- {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.pkg_name}}/__init__.py
@@ -3,7 +3,7 @@
 __author__ = """{{ cookiecutter.full_name }}"""
 __email__ = '{{ cookiecutter.email }}'
 __version__ = '{{ cookiecutter.version }}'
-
+__app_name__ = '{{ cookiecutter.pkg_name }}'
 
 import logging
 from pathlib import Path
